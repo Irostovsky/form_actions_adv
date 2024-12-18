587eed37c19b4067f4d7c58b6af86ff638a5fbaf
Irostovsky <ivan.rostovsky@gmail.com>
2024-12-18 10:31:51 +0200

    wip: FE part of the opinion submittion.

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,24 +1,69 @@
+import { useActionState } from "react";
+
 export function NewOpinion() {
+  const handleSubmit = async (prevState, formData) => {
+    const fields = Object.fromEntries(formData);
+
+    let errors = [];
+    if (fields.body.trim().length < 6) {
+      errors.push("Your opinion should be not less 6 characters");
+    }
+
+    if (errors.length) {
+      return { errors, fields };
+    }
+    return { errors: null, success: true };
+  };
+
+  const [state, formAction] = useActionState(handleSubmit, {
+    errors: null,
+  });
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              required
+              defaultValue={state.fields?.userName}
+            />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              required
+              defaultValue={state.fields?.title}
+            />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            defaultValue={state.fields?.body}
+          ></textarea>
         </p>
 
+        {state.errors && (
+          <ul>
+            {state.errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+        {state.success && <p>Your opinion was submitted!</p>}
         <p className="actions">
           <button type="submit">Submit</button>
         </p>
