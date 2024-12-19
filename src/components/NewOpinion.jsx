e27e5b0c047d9c58844c65e3e51236d49d4201e3
Irostovsky <ivan.rostovsky@gmail.com>
2024-12-19 10:21:31 +0200

    Use form status hook to manage the form pending state.

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,6 +1,7 @@
 import { use } from "react";
 import { useActionState } from "react";
 import { OpinionsContext } from "../store/opinions-context";
+import Submit from "./Submit";
 
 export function NewOpinion() {
   const { addOpinion } = use(OpinionsContext);
@@ -76,11 +77,7 @@ export function NewOpinion() {
           </ul>
         )}
         {formState.success && <p>Your opinion was submitted!</p>}
-        <p className="actions">
-          <button disabled={pending} type="submit">
-            Submit
-          </button>
-        </p>
+        <Submit />
       </form>
     </div>
   );
